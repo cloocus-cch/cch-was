7c2b8a460e8820a07c9f2fc44aad137ad73abf2a
Choi Chan Hoe <cks8483@naver.com>
2022-12-08 18:00:12 +0900

    db구현중

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const mysql = require('promise-mysql');
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
     host: "34.64.230.109",
@@ -12,3 +12,5 @@ const db = mysql.createConnection({
 db.connect();
 
 module.exports = db;
+
+//연결 안되면 구글DB 연결 참고
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,43 +3,32 @@
 const db = require("../config/db");
 
 class UserStorage {
-    static #getUserInfo(data, id){
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users);
-        const userInfo = userKeys.reduce((newUser, info) => {
-            newUser[info] = user[info][idx];
-            return newUser;
-        }, {});
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if (isAll) return users;
-
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
     static getUsers(isAll, ...fields) {
     }
 
     static getUsersInfo(id) {
-        new Promise((resolve, rehect) =>{
-            db.query("SELECT * FROM users id = ?", [id], (err, data) =>{
+        new Promise((resolve, reject) =>{
+            const query ="SELECT * FROM users id = ?"
+            db.query(query, [id], (err, data) =>{
+                if (err) reject(err);
                 console.log(data[0]);
-                resolve(data[0]);//33 7:47
-            });
-        })
+                resolve(data[0]);
+            }); 
+        });
         
     }
 
     static async save(userInfo) {
+        new Promise((resolve, reject) =>{
+            const query ="INSERT INTO users(id, name, psword) VALUES(?, ?, ?);"
+            db.query(query, [userInfo.id, userInfo.name, userInfo.psword], (err) =>{
+                if (err) reject(err);
+                console.log(data[0]);
+                resolve({success: true});
+            }); 
+        });
     }
 }
 
 module.exports = UserStorage;
+
